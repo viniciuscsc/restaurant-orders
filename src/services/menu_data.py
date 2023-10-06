75eb02eefa3d67024fef1be876f7933f4a0450ef
vcscunha <vcs.cunha@icloud.com>
2023-10-06 15:37:46 -0300

    req 03: implementa metodos '__init__' e 'carregar_dados_menu()' da classe 'MenuData'

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -7,26 +7,30 @@ class MenuData:
     def __init__(self, source_path: str) -> None:
         self.source_path = source_path
         self.dishes = set()
-        self.obter_menu()
+        self.carregar_dados_menu()
 
-    def obter_menu(self):
+    def carregar_dados_menu(self):
         with open(self.source_path, "r", newline="") as menu_csv:
-            menu_dict = csv.DictReader(menu_csv)
+            leitor_menu = csv.DictReader(menu_csv)
 
-            for linha in menu_dict:
-                prato = linha["dish"]
-                preco = float(linha["price"])
-                ingrediente = linha["ingredient"]
-                qtd_ingrediente = int(linha["recipe_amount"])
+            for linha in leitor_menu:
+                nome_prato = linha["dish"]
+                preco_prato = float(linha["price"])
+                nome_ingrediente = linha["ingredient"]
+                quantidade_receita = int(linha["recipe_amount"])
 
-                if prato not in self.dishes:
-                    prato_atual = Dish(prato, preco)
-                    self.dishes[prato] = prato_atual
-                    self.dishes.add(prato_atual)
-                else:
-                    prato_atual = self.dishes[prato]
+                prato_existente = None
+                for prato in self.dishes:
+                    if prato.name == nome_prato:
+                        prato_existente = prato
+                        break
 
-                ingrediente_atual = Ingredient(ingrediente)
-                prato_atual.add_ingredient_dependency(
-                    ingrediente_atual, qtd_ingrediente
+                if prato_existente is None:
+                    novo_prato = Dish(nome_prato, preco_prato)
+                    self.dishes.add(novo_prato)
+                    prato_existente = novo_prato
+
+                ingrediente = Ingredient(nome_ingrediente)
+                prato_existente.add_ingredient_dependency(
+                    ingrediente, quantidade_receita
                 )
