8dfffad5a43f608f9dc221dc58593a78aee6f38b
vcscunha <vcs.cunha@icloud.com>
2023-10-06 10:48:34 -0300

    req 02: refatora os dois primeiros testes

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -3,8 +3,18 @@ from src.models.dish import Dish  # noqa: F401, E261, E501
 
 def test_dish():
     # a classe pode ser instanciada
-    lasanha = Dish("lasanha", 20)
+    lasanha = Dish("lasanha", 25.5)
+    outra_lasanha = Dish("lasanha", 25.5)
+    ravioli = Dish("ravioli", 20.5)
+
     assert isinstance(lasanha, Dish)
 
-    # "name" é igual ao passado no construtor
+    # "name" e "price" são iguais ao passado no construtor
     assert lasanha.name == "lasanha"
+    assert lasanha.price == 25.5
+
+    assert outra_lasanha.name == "lasanha"
+    assert outra_lasanha.price == 25.5
+
+    assert ravioli.name == "ravioli"
+    assert ravioli.price == 20.5
